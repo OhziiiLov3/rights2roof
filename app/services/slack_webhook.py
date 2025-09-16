49304dc7d19090784ea1a2e2563f623fa0f1a1a3
ohziiilov3 <klbaskerville0520@gmail.com>
2025-09-16 14:31:13 -0700

    feature: created post request endpoint for slack webook and test simulation for the MCP Client

--- app/services/slack_webhook.py
+++ app/services/slack_webhook.py
@@ -1,10 +1,54 @@
-# slack/fastpi lives 
-# starter code to test locally
+# slack webhook
+from fastapi import FastAPI, Form
+import asyncio
+import os
+import logging
 
-from fastapi import FastAPI
+app = FastAPI(title="Rights-2-Roof Slash Command")
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+
+# Helper Function: Post Threaded response
+async def post_slack_thread(channel_id: str, user_id: str, query_text: str):
+    """
+    For now, just simulates MCP response for testing.
+    """
+    try:
+        logging.info(f"[Right2Roof Bot] simulating pipeline for {user_id}:{query_text}")
+
+#  # ****Run MCP pipeline goes here***
+
+        # simulate final answer
+        final_answer = f"Simulated housing info for query: '{query_text}' "
+        # simulate async posting delay
+        await asyncio.sleep(1)
+
+        logging.info(f"[Right2Roof Bot] Finished simulated response for {user_id}")
+        print(f"[Thread] Channel: {channel_id} | User: {user_id} | Answer: {final_answer}")
+    except Exception as e:
+        logging.exception(f"[HousingBot] Error in simulated pipeline")
+
+
+# Slack Slash Command  Endpoint
+@app.post("/slack/rights-2-roof")
+async def slack_roof(text: str = Form(...),user_id: str = Form(...),channel_id: str = Form(...),):
+    """
+    Handles /rights-2-roof <query> slash command from Slack.
+    Responds immediately, then posts final answer asynchronously.
+    """
+
+    # step 1: Slack to respond immediately 
+    ephemeral_response = {
+        "response_type": "ephemeral",
+        "text": f"Got it! Running Rights2Roof search for: {text}"
+    }
+
+    # step 2: Trigger background task for final answer
+    asyncio.create_task(post_slack_thread(channel_id,user_id, text))
+
+    return ephemeral_response
+
 
 @app.get("/")
-def home():
-    return {"message": "Welcome to Rights2Roof API"}
+def root():
+    return {"message": "Rights2Roof Slack webhook is running"}
