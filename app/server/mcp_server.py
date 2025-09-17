43c29161c8bce717b5d1fc7217b79436ad1afba5
wolfiesarcade <itoliverpeter2@gmail.com>
2025-09-17 17:57:51 -0400

    added planner agent

--- app/server/mcp_server.py
+++ app/server/mcp_server.py
@@ -7,7 +7,11 @@ from app.tools.wikipedia_tools import wikipedia_search
 from app.tools.geo_tools import get_location_from_ip
 from app.tools.tavily_tools import tavily_search
 from app.tools.time_tools import time_tool_fn
+from app.agents.planner_agent import planner_agent
 rights2roof_server = FastMCP("rights2roof_tools")
+
+
+
 
 
 @rights2roof_server.tool(description="geolocation tool to find the users location")
@@ -27,6 +31,23 @@ def time()-> Dict[str, Any]:
 @rights2roof_server.tool(description="Search Tavily for recent/local housing info and return structured output.")
 def tavily(query: str)-> Dict[str, Any]:
     return{"result": tavily_search}
+
+
+
+@rights2roof_server.tool(description="Run Planner agent to break query into steps")
+
+def planner_agent_tool(query: str) -> Dict[str, Any]:
+# simulate final answer
+    plan_result = planner_agent(query)
+
+        # Runs planner agent - just to demo for now - this will change 
+    if hasattr(plan_result, "model_dump"):
+        plan_steps = plan_result.model_dump().get("plan", [])
+    else:
+        plan_steps = plan_result.get("plan", [])
+
+    return{"result": plan_result}
+
 
 def ping() -> str:
     return "pong"
