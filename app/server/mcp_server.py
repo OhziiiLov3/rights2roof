204575258d366f59181133f11643bddc126f7bde
ohziiilov3 <klbaskerville0520@gmail.com>
2025-09-17 10:58:19 -0700

    add: pull mcp_server into branch

--- app/server/mcp_server.py
+++ app/server/mcp_server.py
@@ -7,6 +7,7 @@ from app.tools.wikipedia_tools import wikipedia_search
 from app.tools.geo_tools import get_location_from_ip
 from app.tools.tavily_tools import tavily_search
 from app.tools.time_tools import time_tool_fn
+
 rights2roof_server = FastMCP("rights2roof_tools")
 
 
@@ -14,13 +15,9 @@ rights2roof_server = FastMCP("rights2roof_tools")
 def fetch_location_from_ip(ip:Optional[str] = None) -> Dict[str, Any]:
     return{"result": get_location_from_ip(ip) }
 
-
-
-
 @rights2roof_server.tool(description="Search Wikipedia for information")
 def wikipedia_lookup(query: str) -> Dict[str, Any]:
     return {"result": wikipedia_search(query)}
-
 
 @rights2roof_server.tool(description="Return the current date and time in ISO format.")
 def time()-> Dict[str, Any]:
@@ -29,6 +26,9 @@ def time()-> Dict[str, Any]:
 @rights2roof_server.tool(description="Search Tavily for recent/local housing info and return structured output.")
 def tavily(query: str)-> Dict[str, Any]:
     return{"result": tavily_search}
+
+
+
 
 def ping() -> str:
     return "pong"
