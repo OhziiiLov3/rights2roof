53db4926b2bf8798d632dfafa7b6edcfe9b40357
wolfiesarcade <itoliverpeter2@gmail.com>
2025-09-16 20:27:55 -0400

    adjusted MCP server to use tools folder

--- app/server/mcp_server.py
+++ app/server/mcp_server.py
@@ -1,53 +1,23 @@
 #MCP Server goes here
-from fastmcp import FastMCP
-from langchain_community.tools import WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
-from typing import Optional
-import requests
-from typing import Dict, Any
-# from app.models.schemas import ToolOutput
-import os
-
+from fastmcp import FastMCP            
+from typing import Optional, Dict, Any  
+import requests                         
+import os    
+from app.tools.wikipedia_tools import wikipedia_search
+from app.tools.geo_tools import get_location_from_ip
 rights2roof_server = FastMCP("rights2roof_tools")
-wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
-@rights2roof_server.tool()
-def get_location_from_ip(ip:Optional[str] = None) -> Dict[str, Any]:
-    """
-    Uses ip-api.com to get location info from IP.
-    If no IP provided, falls back to server IP.
-    """
 
-    if not ip:
-        ip = os.getenv("IP_ADDRESS")
-    url = f"http://ip-api.com/json/{ip}"
-    response = requests.get(url)
-    data = response.json()
-
-    if data.get("status") != "success":
-        raise ValueError(f"Could not get location for IP {ip}")
-    return{
-        "tool": "geo_location",
-        "input": {"ip": ip},
-        "output" : {"city": data.get("city"), "state": data.get("region"), "country": data.get("country")},
-        "step":"Get User Location"
-    }
+@rights2roof_server.tool(description="geolocation tool to find the users location")
+def fetch_location_from_ip(ip:Optional[str] = None) -> Dict[str, Any]:
+    return{"result": get_location_from_ip(ip) }
 
 
 
 
-@rights2roof_server.tool()
-def wikipedia_search(query: str) -> Dict[str, Any]:
-    """Search Wikipedia for a given topic and return the result text."""
-    result = wikipedia.run(query)
-    return {
-        "tool":"wikipedia_search",
-        "input":{"query": query},
-        "output":result,
-        "step":"Look up update to Rental Laws and Affordable Housing information"
-
-    }
-
+@rights2roof_server.tool(description="Search Wikipedia for information")
+def wikipedia_lookup(query: str) -> Dict[str, Any]:
+    return {"result": wikipedia_search(query)}
 
 
 
