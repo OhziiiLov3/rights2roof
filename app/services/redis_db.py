ddba97bae789efb9790405fff84f25528c9849d8
Don Namgyal <don.namgyal@hotmail.com>
2025-09-21 22:31:55 -0400

    initial redis setup

--- app/services/redis_db.py
+++ app/services/redis_db.py
@@ -16,11 +16,13 @@ def create_user_session(user_id, user_data: UserData) -> UserData:
         r.set(user_id, user_data)
     except redis.RedisError as e:
         print(f"Error creating session for user {user_id}: {e}")
+        return None
 
 def update_user_session(user_id, user_data: UserData) -> UserData:
     try:
         r.set(user_id, user_data)
     except redis.RedisError as e:
         print(f"Error updating session for user {user_id}: {e}")
+        return None
 
 
