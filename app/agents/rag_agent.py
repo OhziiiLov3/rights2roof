8c292127295e78209ade03c8c15e5427348a1fdb
Don Namgyal <don.namgyal@hotmail.com>
2025-09-23 11:14:54 -0400

    update rag

--- app/agents/rag_agent.py
+++ app/agents/rag_agent.py
@@ -14,10 +14,12 @@ import redis
 from langchain_openai import OpenAIEmbeddings
 from langchain_redis import RedisConfig, RedisVectorStore
 from langchain_community.document_loaders import PyPDFLoader
+from pydantic import BaseModel, Field
 # from langchain_core.output_parsers import PydanticOutputParser
 # from app.models.schemas import RAGAgentResponse
 
 load_dotenv()
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, verbose=True)
 DIRECTORY_PATH = "app/resources/files"
 
 REDIS_URL=os.getenv("REDIS_URL", "redis://localhost:32771")
@@ -34,39 +36,30 @@ config = RedisConfig(
     embedding=embeddings
 )
 
-#create vector store in Redis
+#initialize the vector store and retreiver
 vector_store = RedisVectorStore(embeddings, config=config)
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
-def create_vector_store():
-    for filename in os.listdir(DIRECTORY_PATH):
-        if filename.endswith(".pdf"):
-            file_path = os.path.join(DIRECTORY_PATH, filename)
-            loader = PyPDFLoader(file_path)
-            pages = loader.load_and_split()
-            vector_store.add_documents(documents=pages)
-            print(f"added {filename} to vector store")
-
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, verbose=True)
-# rag_agent_response = PydanticOutputParser(pydantic_object=RAGAgentResponse)
 
 #Base prompt for RAG agent
 system_prompt = """
-You are a helpful RAG agent that answers questions about the the laws and regulation regarding the rental housing market in California and New York.
-Your answers should be detailed and include specific references to the relevant laws and regulations.
-Please ONLY use the context provided to you. Do not use any other context.
-If you don't know the answer, just say that you don't know. DO NOT try to make up an answer.
-"""
+    You are a helpful RAG agent that answers questions about the the laws and regulation regarding the rental housing market in California and New York.
+    Your answers should be detailed and include specific references to the relevant laws and regulations.
+    Please ONLY use the context provided to you. Do not use any other context.
+    If you don't know the answer, just say that you don't know. DO NOT try to make up an answer.
+    """
+#You must provide the response in the the format defined by the RAGAgentResponse schema. 
 
 prompt_template = ChatPromptTemplate.from_messages([
     SystemMessage(content=system_prompt),
     ("human", "{plan} {query} {context}")
 ])
 
+# rag_agent_response = PydanticOutputParser(pydantic_object=RAGAgentResponse)
+# rag_chain = prompt_template | llm | rag_agent_response
+
+#llm chain to process the prompt and generate a response
 rag_chain = prompt_template | llm | StrOutputParser()
-
-query = "Can a landlord in NYC evict a tenant for not paying rent during a pandemic?"
-retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
-
 
 @traceable
 def rag_agent(plan: list, query: str):
@@ -76,3 +69,5 @@ def rag_agent(plan: list, query: str):
         return result
     except Exception as e:
         return {"error": str(e)}
+
+print(rag_agent(["Step 1: Use the vector store to get relevant context based on the user's query."], "What are the tenant rights in NYC regarding rent increases?"))
