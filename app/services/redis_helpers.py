0ca5ef369f010fe45f9c28e778d82a2dad9f53ed
ohziiilov3 <klbaskerville0520@gmail.com>
2025-09-26 14:44:41 -0700

    deploy

--- app/services/redis_helpers.py
+++ app/services/redis_helpers.py
@@ -10,6 +10,7 @@ from typing import List , Any, Optional
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST","localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.environ.get("REDIS_PASSWORD", None),
     db=0,
     decode_responses=True
 )
